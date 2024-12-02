433ac08e626663deabe161a0313108b42a5deb75
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2024-12-02 10:36:43 -0300

    Intialize API

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+vide_post_args = reqparse.RequestParser()
+vide_post_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+vide_post_args.add_argument("views", type=int, help="Views of the video")
+vide_post_args.add_argument("likes", type=int, help="Likes in the video")
+
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    
+    def post(self, video_id):
+        args = vide_post_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+api.add_resource(Video, "/video/<int:video_id>") 
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+response = requests.post(BASE + "video/1", {"name": "my video", "likes": 10})
+print(response.json())
+
+input()
+
+response = requests.get(BASE + "video/1")
+print(response.json())
