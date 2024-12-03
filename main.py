4ef04cc865a37fb3615d625383e1af1d9ad9e753
Letícia <leticia.c.rodrigues0@gmail.com>
2024-12-03 10:44:36 -0300

    DB integration pending

--- main.py
+++ main.py
@@ -4,27 +4,40 @@ from flask_restful import Api, Resource, reqparse, abort
 app = Flask(__name__)
 api = Api(app)
 
-vide_post_args = reqparse.RequestParser()
-vide_post_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-vide_post_args.add_argument("views", type=int, help="Views of the video")
-vide_post_args.add_argument("likes", type=int, help="Likes in the video")
+video_post_args = reqparse.RequestParser()
+video_post_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_post_args.add_argument("views", type=int, help="Views of the video")
+video_post_args.add_argument("likes", type=int, help="Likes in the video")
 
 videos = {}
 
-def check_video_id(video_id):
+def abort_if_video_id_doesnt_exists(video_id):
     if video_id not in videos:
         abort(404, message="Could not find video.")
+
+def abort_if_video_id_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already existis with that ID.")
 
     
 class Video(Resource):
     def get(self, video_id):
-        check_video_id(video_id)
+        abort_if_video_id_doesnt_exists(video_id)
         return videos[video_id]
     
     def post(self, video_id):
-        args = vide_post_args.parse_args()
+        abort_if_video_id_exists(video_id)
+
+        args = video_post_args.parse_args()
+        
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exists(video_id)
+
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>") 
 
--- test.py
+++ test.py
@@ -2,8 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "video/1", {"name": "my video", "likes": 10})
-print(response.json())
+data = [
+    {"name": "video 1", "likes": 10, "views": 10000},
+    {"name": "video 2", "likes": 20, "views": 10000},
+    {"name": "video 3", "likes": 30, "views": 10000},
+]
+
+for i in range(len(data)):
+    response = requests.post(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+
+response = requests.delete(BASE + "video/0")
+print(response)
 
 input()
 
